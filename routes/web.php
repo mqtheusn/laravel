768cabbcf3f6f997a3e51d8e24d16b999661aa03
mqtheusn <matheuszor14@gmail.com>
2025-02-25 20:12:27 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[\App\Http\Controllers\PrincipalController::class,'principal']);
+Route::get('/contato',[\App\Http\Controllers\PrincipalController::class,'contato']);
+Route::get('/sobre-nos',[\App\Http\Controllers\PrincipalController::class,'sobreNos']);
+
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+Route::get('/clientes', function () {
+    return view('welcome');
+});
+
+Route::get('/fornecedores', function () {
+    return view('welcome');
+});
