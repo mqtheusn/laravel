ebdfad7fce1485d9e62192079d2445b721fedda1
mqtheusn <matheuszor14@gmail.com>
2025-02-25 20:19:20 -0300

    .

---
+++ resources/views/site/contato.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    <li><a href="{{route('site.principal')}}"></a>Principal</li>
+    <li><a href="{{route('site.contato')}}"></a>Contato</li>
+    <li><a href="{{route('site.sobre-nos')}}"></a>Sobre nós</li>
+        
+</ul>
---
+++ resources/views/site/principal.blade.php
@@ -0,0 +1,8 @@
+<h3>principal - (view)</h3>
+
+<ul>
+    <li><a href="{{route('site.principal')}}"></a>Principal</li>
+    <li><a href="{{route('site.contato')}}"></a>Contato</li>
+    <li><a href="{{route('site.sobre-nos')}}"></a>Sobre nós</li>
+        
+</ul>
